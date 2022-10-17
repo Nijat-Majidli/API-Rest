dde0fbee9585ed30044361bbce7e868f856e5246
Majidli Nijat <nijatmajidli@gmail.com>
2022-10-18 00:26:01 +0200

    modif

--- BDD.sql
+++ BDD.sql
@@ -15,8 +15,7 @@ CREATE TABLE users(
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 INSERT INTO `users` (`user_id`, `user_nom_prenom`, `user_tel`, `user_email`, `user_adresse`, `user_cp`, `user_ville`, `user_pays`) 
-VALUES 
-(1, 'Richard Jules', '07 51 25 08 10', 'richard@gmail.com', '5 rue Général Leclerc', 80000, 'Amiens', 'France'),
+VALUES (1, 'Richard Jules', '07 51 25 08 10', 'richard@gmail.com', '5 rue Général Leclerc', 80000, 'Amiens', 'France'),
 (2, 'Paul Michel', '07 50 27 81 18', 'p.michel@yahoo.fr', '52 rue Jean Jaures', 75000, 'Paris', 'France'),
 (3, 'Mathieu Bompart', '01 23 45 67 89', 'bompart@homail.com', '836 rue du Mas de Verchant', 34000, 'Montpellier', 'France'),
 (4, 'Celine Périk', '09 87 65 43 21', 'c-perik@gmail.com', '15 rue Général Leclerc', 13000, 'Marseille', 'France'),
